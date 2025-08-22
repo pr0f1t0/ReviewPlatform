f4d952c8436e7d75621f6bf6ec4792e927e08aad
pr0f1t0 <bohdandvibororchyn@gmail.com>
2025-08-23 00:34:41 +0200

    Implement photo download

--- src/main/java/com/pr0f1t/ReviewPlatform/api/controller/PhotoController.java
+++ src/main/java/com/pr0f1t/ReviewPlatform/api/controller/PhotoController.java
@@ -5,6 +5,10 @@ import com.pr0f1t.ReviewPlatform.core.domain.mapper.PhotoMapper;
 import com.pr0f1t.ReviewPlatform.core.port.out.PhotoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,6 +30,21 @@ public class PhotoController {
         return photoService.uploadPhotoAsync(file)
                 .thenApply(photoMapper::toDto)
                 .thenApply(photoDto -> ResponseEntity.ok(photoDto));
+
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public CompletableFuture<ResponseEntity<Resource>> getPhoto(@PathVariable String id) {
+
+        return photoService.getPhotoAsResourceAsync(id).thenApply(photoResource ->
+                photoResource.map(photo ->
+                        ResponseEntity.ok()
+                                .contentType(MediaTypeFactory.getMediaType(photo)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM))
+                                .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                                .body(photo)
+                ).orElse(ResponseEntity.notFound().build())
+        );
 
     }
 }
